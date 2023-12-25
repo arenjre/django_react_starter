df60b6734b1091d263ea2256e257afa7b5746008
Deependra Singh <deepenrasg@gmail.com>
2023-12-25 12:11:43 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <h3>My name is Khan, and i'm not a terrorist</h3>
+    </>
+  )
+}
+
+export default App
