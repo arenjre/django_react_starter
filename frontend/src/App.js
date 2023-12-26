f1cc8da0cf35c39430a9f75991a1ea2d4b05acca
Deependra Singh <deepenrasg@gmail.com>
2023-12-26 19:02:43 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <h1>Hello, World!</h1>
-      <h3>My name is Khan, and i'm not a terrorist</h3>
+      <h3>Welcome to the coder world</h3>
     </>
   )
 }
